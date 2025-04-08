c838ae53f37edbd3669063045dc7102571acda8d
Gabriel <lakowskigabriel@gmail.com>
2025-04-08 11:43:39 +0200

    First Steps API

--- data_analysis/analize.py
+++ data_analysis/analize.py
@@ -19,7 +19,7 @@ def connection():
     print(tables)
     if con is not None:
         print('Connection successful')
-    
+        return con
     else:
         print('Connection failed')
     
@@ -29,115 +29,237 @@ def all_data():
     data = con.execute('SELECT * FROM countries').fetchall()
     return jsonify(data)
 
-@app.route('/age', methods=['GET'])
-def age():
-    con = connection()
-
-    age = request.args.get('age')
-    country = request.args.get('country')
+@app.route('/AgeCategory', methods=['GET'])
+def AgeCategory():
     
-    query = "SELECT * FROM AgeCategory"
-    params = []
-
-    if age and country:
-        query += " WHERE age = ? AND country = ?"
-        params.extend([age, country])
-        data = con.execute(query, params).fetchall()
-    elif age:
-        query += " WHERE age = ?"
-        params.append(age)
-        data = con.execute(query, params).fetchall()
-    elif country:
-        query += " WHERE country = ?"
-        params.append(country)
-        data = con.execute(query, params).fetchall()
+    con = connection()
+    
+    age_ids = request.args.get('age_ids')    
+    print(age_ids)
+    
+    query = "SELECT * FROM Indicators INNER JOIN AgeCategory ON Indicators.age_category_id = AgeCategory.Id"
+    
+    if age_ids:
+         
+        query += (f" WHERE ageCategory.Id IN ({age_ids}) LIMIT 10")
+        
+        print(query)
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+        
     else:
         data = con.execute(query).fetchall()
-    return jsonify(data)
+        print(data)
+        return jsonify(data)    
 
-@app.route('/general_health', methods=['GET'])
-def general_health():
-    con = connection()
-
-    health = request.args.get('health')
-    params = []
-    query = "SELECT * FROM GeneralHealth"
-    if health:
-        query += " WHERE health = ?"
-        params.append(health)
-        data = con.execute(query, params).fetchall()
-    else:
-        data = con.execute(query)
-    return jsonify(data)
-
-@app.route('/EcigaretteUsage', methods=['GET'])
-def ecigarette_usage():
-    con = connection()
 
     
-    query = "SELECT * FROM EcigaretteUsage"
-    params = []
-
 @app.route('/CovidPos', methods=['GET'])
-def covid_pos():
+def CovidPos():
+    
     con = connection()
+    
+    CovidPos_ids = str(request.args.get('CovidPos_ids'))
+    print(CovidPos_ids)
+    
+    query = "SELECT * FROM Indicators INNER JOIN CovidPos ON Indicators.covid_pos_id = CovidPos.Id"
+    
+    if CovidPos_ids:
+        
+        query += (f" WHERE CovidPos.Id IN ({CovidPos_ids}) LIMIT 10")
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+    else:
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+        
 
-    query = "SELECT * FROM CovidPos"
-    params = []
 
+@app.route('/ECigaretteUsage', methods=['GET'])
+def ECigaretteUsage():
+    
+    con = connection()
+    
+    ECigaretteUsage_ids = str(request.args.get('ECIgaretteUsage_ids'))
+    
+    query = "SELECT * FROM Indicators INNER JOIN ECigaretteUsage ON ECigaretteUsage.Id = Indicators.ecigarette_usage_id"
+
+    if ECigaretteUsage:
+        
+        query += (f" WHERE ECigaretteUsage.Id IN ({ECigaretteUsage_ids}) LIMIT 10")
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+    else:
+        
+        data = con.execute(quety).fetchall()
+        print(data)
+        return jsonify(data)
+    
+@app.route('/GeneralHralth', methods=['GET'])
+def GeneralHralth():
+    
+    con = connection()
+    
+    GeneralHralth_ids = str(request.args.get('GeneralHralth_ids'))
+    
+    query = "SELECT * FROM Indicators INNER JOIN GeneralHralth ON GeneralHralth.Id = Indicators.general_health_id"
+
+    if GeneralHralth_ids:
+        
+        query += (f" WHERE GeneralHralth.Id IN ({GeneralHralth_ids}) LIMIT 10")
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+    else:
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
 @app.route('/HadDiabetes', methods=['GET'])
-def had_diabetes():
+def HadDiabetes():
+    
     con = connection()
+    
+    HadDiabetes_ids = str(request.args.get('HadDiabetes_ids'))
+    
+    query = "SELECT * FROM Indicators INNER JOIN HadDiabetes ON HadDiabetes.Id = Indicators.had_diabetes_id"
 
-    query = "SELECT * FROM HadDiabetes"
-    params = []
-
-@app.route('/Indicators', methods=['GET'])
-def indicators():
-    con = connection()
-
-    query = "SELECT * FROM Indicators"
-    params = []
-
+    if HadDiabetes_ids:
+        
+        query += (f" WHERE HadDiabetes.Id IN ({HadDiabetes_ids}) LIMIT 10")
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+    else:
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
 @app.route('/RaceEthnicityCategory', methods=['GET'])
-def race_ethnicity_category():
+def RaceEthnicityCategory():
+    
     con = connection()
-
-    query = "SELECT * FROM RaceEthnicityCategory"
-    params = []
-
+    
+    RaceEthnicityCategory_ids = str(request.args.get('RaceEthnicityCategory_ids'))
+    
+    query = "SELECT * FROM Indicators INNER JOIN RaceEthnicityCategory_ids ON RaceEthnicityCategory.Id = Indicators.race_ethnicity_category_id"
+    
+    if RaceEthnicityCategory:
+        
+        query += (f" WHERE RaceEthnicityCategory.Id IN ({RaceEthnicityCategory_ids}) LIMIT 10")
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
 @app.route('/RemovedTeeth', methods=['GET'])
-def removed_teeth():
+def RemovedTeeth():
+    
     con = connection()
+    
+    RemovedTeeth_ids = str(request.args.get('RemovedTeeth_ids'))
+    
+    query = "SELECT * FROM Indicators INNER JOIN RemovedTeeth ON RemovedTeeth.Id = Indicators.removed_teeth_id"
 
-    query = "SELECT * FROM RemovedTeeth"
-    params = []
+    if RemovedTeeth_ids:
+        
+        query += (f" WHERE RemovedTeeth.Id IN ({RemovedTeeth_ids}) LIMIT 10")
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+    else:
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
 
 @app.route('/SmokerStatus', methods=['GET'])
-def smoker_status():
+def SmokerStatus():
+    
     con = connection()
+    
+    SmokerStatus_ids = str(request.args.get('SmokerStatus_ids'))
+    
+    query = "SELECT * FROM Indicators INNER JOIN SmokerStatus ON SmokerStatus.Id = Indicators.smoker_status_id"
 
-    query = "SELECT * FROM SmokerStatus"
-    params = []
-
-@app.route('/States', methods=['GET'])
-def states():
+    if SmokerStatus_ids:
+        
+        query += (f" WHERE SmokerStatus.Id IN ({SmokerStatus_ids}) LIMIT 10")
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+    else:
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+@app.route('/State' , methods=['GET'])
+def State():
+    
     con = connection()
+    
+    State_ids = str(request.args.get('State_ids'))
+    
+    query = "SELECT * FROM Indicators INNER JOIN State ON State.Id = Indicators.state_id"
 
-    query = "SELECT * FROM States"
-    params = []
-
+    if State_ids:
+        
+        query += (f" WHERE State.Id IN ({State_ids}) LIMIT 10")
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+    else:
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
 @app.route('/TetanusLast10Tdap', methods=['GET'])
-def tetanus_last_10_tdap():
+def TetanusLast10Tdap():
+    
     con = connection()
+    
+    TetanusLast10Tdap_ids = str(request.args.get('TetanusLast10Tdap_ids'))
+    
+    query = "SELECT * FROM Indicators INNER JOIN TetanusLast10Tdap ON TetanusLast10Tdap.Id = Indicators.tetanus_last_10_tdap_id"
 
-    query = "SELECT * FROM TetanusLast10Tdap"
-    params = []
+    if TetanusLast10Tdap_ids:
+        
+        query += (f" WHERE TetanusLast10Tdap.Id IN ({TetanusLast10Tdap_ids}) LIMIT 10")
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+    else:
+        
+        data = con.execute(query).fetchall()
+        print(data)
+        return jsonify(data)
+    
+        
+app.run(debug=True) 
 
 
-
-
-
-# app.run(debug=True) 
-
-connection()
