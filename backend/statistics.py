9f91fd50fc7e3ff4bb31738e3125cebc4e5ab119
KawalChlopa <lakowskigabriel@gmail.com>
2025-06-01 16:16:31 +0200

    dict mapping

--- backend/statistics.py
+++ backend/statistics.py
@@ -2,40 +2,29 @@ import duckdb as dd
 from flask import Flask, jsonify, request
 from persistence import db_location
 
-def connection():
-    path = db_location.get_location()
-    print(f"Database path: {path}")
-    con = dd.connect(path)
-    if con is not None:
-        print('Connection successful')
-        return con
-    else:
-        print('Connection failed')
 
+special_case = {
+    'state_id': 'State',
+    'age_category_id': 'AgeCategory'
+}
 
 def average(con, columns, group=None):
     select_clause = ', '.join([f'ROUND(AVG({col}), 2) as average_{col}' for col in columns])
     query = f"""
-    SELECT {select_clause}
+    SELECT {group}, {select_clause} 
     FROM Indicators 
     WHERE had_heart_attack = 1 
     """
-    if group == 'state_id':
+    if group in special_case:
         query = f"""
-        SELECT s.name, {select_clause}
+        SELECT x.name, {select_clause}
         FROM Indicators i
-        JOIN State s ON i.state_id = s.id
+        JOIN {special_case[group]} x ON i.{group} = x.id
         WHERE had_heart_attack = 1
-        GROUP BY s.name
+        GROUP BY x.name
         """
-    elif group == 'age_category_id':
-        query = f"""
-        SELECT s.name, {select_clause}
-        FROM Indicators i
-        JOIN AgeCategory s ON i.age_category_id = s.id
-        WHERE had_heart_attack = 1
-        GROUP BY s.name
-        """
+
+
 
     print(f"Executing query: {query}")
     result = con.execute(query)
@@ -49,21 +38,13 @@ def median(con, columns, group=None):
     WHERE had_heart_attack = 1
     """
 
-    if group == 'state_id':
+    if group in special_case:
         query = f"""
-        SELECT s.name as State, {select_clause}
+        SELECT x.name, {select_clause}
         FROM Indicators i
-        JOIN State s ON i.state_id = s.id
+        JOIN {special_case[group]} x ON i.{group} = x.id
         WHERE had_heart_attack = 1
-        GROUP BY s.name
-        """
-    elif group == 'age_category_id':
-        query = f"""
-        SELECT ac.name as AgeCategory, {select_clause}
-        FROM Indicators i
-        JOIN AgeCategory ac ON i.age_category_id = ac.id
-        WHERE had_heart_attack = 1
-        GROUP BY ac.name
+        GROUP BY x.name
         """
 
     print(f"Executing query: {query}")
@@ -78,21 +59,13 @@ def standard_deviation(con, columns, group=None):
     FROM Indicators
     WHERE had_heart_attack = 1
     """
-    if group == 'state_id':
+    if group in special_case:
         query = f"""
-        SELECT s.name as State, {select_clause}
+        SELECT x.name, {select_clause}
         FROM Indicators i
-        JOIN State s ON i.state_id = s.id
+        JOIN {special_case[group]} x ON i.{group} = x.id
         WHERE had_heart_attack = 1
-        GROUP BY s.name
-        """
-    elif group == 'age_category_id':
-        query = f"""
-        SELECT ac.name as AgeCategory, {select_clause}
-        FROM Indicators i
-        JOIN AgeCategory ac ON i.age_category_id = ac.id
-        WHERE had_heart_attack = 1
-        GROUP BY ac.name
+        GROUP BY x.name
         """
     print(f"Executing query: {query}")
     result = con.execute(query)
@@ -105,21 +78,13 @@ def max_value(con, columns, group=None):
     FROM Indicators
     WHERE had_heart_attack = 1
     """
-    if group == 'state_id':
+    if group in special_case:
         query = f"""
-        SELECT s.name as State, {select_clause}
+        SELECT x.name, {select_clause}
         FROM Indicators i
-        JOIN State s ON i.state_id = s.id
+        JOIN {special_case[group]} x ON i.{group} = x.id
         WHERE had_heart_attack = 1
-        GROUP BY s.name
-        """
-    elif group == 'age_category_id':
-        query = f"""
-        SELECT ac.name as AgeCategory, {select_clause}
-        FROM Indicators i
-        JOIN AgeCategory ac ON i.age_category_id = ac.id
-        WHERE had_heart_attack = 1
-        GROUP BY ac.name
+        GROUP BY x.name
         """
 
     print(f"Executing query: {query}")
@@ -133,21 +98,13 @@ def min_value(con, columns, group=None):
     FROM Indicators
     WHERE had_heart_attack = 1
     """
-    if group == 'state_id':
+    if group in special_case:
         query = f"""
-        SELECT s.name as State, {select_clause}
+        SELECT x.name, {select_clause}
         FROM Indicators i
-        JOIN State s ON i.state_id = s.id
+        JOIN {special_case[group]} x ON i.{group} = x.id
         WHERE had_heart_attack = 1
-        GROUP BY s.name
-        """
-    elif group == 'age_category_id':
-        query = f"""
-        SELECT ac.name as AgeCategory, {select_clause}
-        FROM Indicators i
-        JOIN AgeCategory ac ON i.age_category_id = ac.id
-        WHERE had_heart_attack = 1
-        GROUP BY ac.name
+        GROUP BY x.name
         """
 
     print(f"Executing query: {query}")
@@ -155,24 +112,17 @@ def min_value(con, columns, group=None):
     return result
 
 def percentile(con, columns, group=None):
-    select_clause = ', '.join([f"ROUND(SUM(CASE WHEN {col} = TRUE THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 3) as percent_{col}" for col in columns])
+    select_clause = ', '.join([f"ROUND(SUM(CASE WHEN {col} = TRUE THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) as percent_{col}" for col in columns])
     query = f"""
     SELECT {select_clause}
     FROM Indicators
     """
-    if group == 'state_id':
+    if group in special_case:
         query = f"""
-        SELECT s.name as State, {select_clause}
+        SELECT x.name, {select_clause}
         FROM Indicators i
-        JOIN State s ON i.state_id = s.id
-        GROUP BY s.name
-        """
-    elif group == 'age_category_id':
-        query = f"""
-        SELECT ac.name as AgeCategory, {select_clause}
-        FROM Indicators i
-        JOIN AgeCategory ac ON i.age_category_id = ac.id
-        GROUP BY ac.name
+        JOIN {special_case[group]} x ON i.{group} = x.id
+        GROUP BY x.name
         """
 
     print(f"Executing query: {query}")
